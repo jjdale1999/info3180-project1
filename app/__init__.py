c5b0f801149e2af7cfb4bdf69987c09a7df30b4a
jjdale1999 <dalejordanne@gmail.com>
2020-03-13 18:40:49 -0500

    everything

--- app/__init__.py
+++ app/__init__.py
@@ -7,7 +7,6 @@ SQLALCHEMY_TRACK_MODIFICATIONS = False
 UPLOAD_FOLDER = "./app/static/uploads"
 SECRET_KEY = 'Sup3r$3cretkey'
 SQLALCHEMY_DATABASE_URI='postgres://ljdaplxbetluhc:220d2f83502f7238b071107768169c5f59b3077fa78805c794311d0d60fe6c28@ec2-34-206-252-187.compute-1.amazonaws.com:5432/dee35cq710ood1'
-
 app.config.from_object(__name__)
 
 db = SQLAlchemy(app)
